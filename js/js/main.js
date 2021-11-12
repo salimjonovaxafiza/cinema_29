ab4e33ac5155511a56a66aa1170916b99212114b
salimjonovaxafiza <salimjonovaxafiza@gmail.com>
2021-11-12 14:25:42 +0500

    TZ-14

---
+++ js/js/main.js
@@ -0,0 +1,51 @@
+// console.log('Hello')
+// let name="Xafiza"
+// console.log(name)
+// const AGE = 17;
+// let Age = 7;
+// // AGE = 40;
+// Age=90;
+// console.log('before:', person);
+// var person='shaxs';
+// console.log('after:',person);
+// console.log('before:', person);
+// let person='shaxs';
+// console.log('after:',person);
+// let Xafiza=20;
+// typeof(Xafiza)
+// let person = {
+//     name:'Xafiza', 
+//     age: 17,
+//     height: 1.60,
+// }
+console.log (99 / 3);
+console.log (2 / 4);
+console.log (144 / 0);
+console.log ('10' / 2);
+console.log ('12' / 5);
+console.log (23/Infinity);
+console.log ('22' * 2);
+console.log ('one' * 2);
+console.log (0.5 * 2);
+console.log (.2 * 2);
+console.log ('3' + 2);
+console.log (2 + + '3');
+console.log (12 + 2);
+console.log ('23' - '3');
+console.log ('23'- 3);
+let year = 2021;
+let age = 34;
+console.log("Я родился в ", year - age, "году");
+
+console.log('')
+let one = '1';
+let otherOne = 1;
+
+console.log(Boolean(144));
+console.log(Boolean(0));
+console.log(Boolean(true));
+console.log(Boolean(false));
+console.log(Boolean({}));
+console.log(Boolean({name:'Вася'}));
+console.log(Boolean([]));
+console.log(Boolean([1, 2, 3]));
